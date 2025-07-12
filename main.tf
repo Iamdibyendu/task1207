382e929adf4d1a401d38e73d86497e772a9bdb23
Iamdibyendu <dibyenduadhikary57@gmail.com>
2025-07-12 18:03:21 +0530

    commit new code add

--- main.tf
+++ main.tf
@@ -2,3 +2,7 @@ resource "azurerm_resource_group" "rgtask" {
   name     = "rg-dibyendu"
   location = "central india"
 }
+resource "azurerm_resource_group" "rgtask1" {
+  name     = "rg-dibyendu-102"
+  location = "central india"
+}
